ec0c584d7e26a7e54c838841381d087eb3063f6c
nut-parithat <starsis25391@gmail.com>
2022-04-27 14:52:11 +0700

    feature

---
+++ route/Test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+import 'videojs-contrib-dash'
+import 'videojs-contrib-hls'
+import 'videojs-contrib-eme'
+
+export const VideoJS = (props) => {
+  const videoRef = React.useRef(null);
+  const playerRef = React.useRef(null);
+  const { options, onReady } = props;
+
+  React.useEffect(() => {
+
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      const videoElement = videoRef.current;
+
+      if (!videoElement) return;
+
+      const player = playerRef.current = videojs(videoElement, options, () => {
+        // player.log('player is ready');
+        player.eme()
+        onReady && onReady(player);
+      });
+
+      // You can update player in the `else` block here, for example:
+    } else {
+      // player.autoplay(options.autoplay);
+      // player.src(options.sources);
+    }
+  }, [options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  React.useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div >
+
+      <div data-vjs-player>
+        <video
+          style={{ width: '100%' }}
+          // controls
+          ref={videoRef}
+          className="video-js vjs-default-skin vjs-big-play-centered"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default VideoJS;
---
+++ route/TestIndex.js
@@ -0,0 +1,158 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
+import videojs, { type as VideojsType } from 'video.js'
+
+import dynamic from 'next/dynamic'
+const Test = dynamic(() => import('./Test'), { ssr: false })
+
+export default function Home() {
+  const playerRef = React.useRef(null);
+  const [url, setUrl] = useState('')
+  const [license, setLicense] = useState('')
+  const [isSSR, setIsSSR] = useState(true);
+
+  const videoJsOptions = {
+    autoplay: false,
+    // controlBar: {
+    //   children: [
+    //     'qualitySelector',
+    //     'playToggle',
+    //     'volumePanel',
+    //     'currentTimeDisplay',
+    //     'timeDivider',
+    //     'durationDisplay',
+    //     'progressControl',
+    //     'liveDisplay',
+    //     'seekToLive',
+    //     'remainingTimeDisplay',
+    //     'customControlSpacer',
+    //     'playbackRateMenuButton',
+    //     'chaptersButton',
+    //     'descriptionsButton',
+    //     'subsCapsButton',
+    //     'audioTrackButton',
+    //     'fullscreenToggle'
+    //   ],
+    // },
+    // muted: true,
+    // play:3,
+    height: 100,
+    width: 100,
+    // videoWidth:960,videoHeight:400,
+    controls: true,
+    fluid: true,
+    responsive: true,
+    preload: "auto",
+    // enableSourceset: true,
+    liveui: true,
+    // qualityMenu: true,
+    html5: {
+      dash: {
+        useTTML: true
+      },
+      vhs: {
+        withCredentials: false,
+        overrideNative: !videojs?.browser?.IS_SAFARI
+      },
+      nativeCaptions: false,
+      nativeAudioTracks: false,
+      nativeVideoTracks: false
+    },
+    sources: []
+  }
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      player.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      player.log('player will dispose');
+    });
+
+    player.on('ready', () => {
+      console.log('player will readyreadyready');
+    })
+
+    player.on('canplay', () => {
+      console.log('player will canplay', playerRef.current);
+
+    })
+  };
+
+
+  const onTestStream = (valueType) => {
+    if (!playerRef.current) return
+
+    if (!videojs?.browser?.IS_SAFARI) {
+      if (valueType === 'dash') {
+        playerRef.current.src({
+          src: url,
+          type: "application/dash+xml",
+          keySystemOptions: [
+            {
+              name: 'com.widevine.alpha',
+              options: {
+                licenseUrl: license,
+              },
+            }
+          ]
+        })
+      } else if (valueType === 'hls') {
+        playerRef.current.src({
+          src: url,
+          type: "application/x-mpegURL",
+          // keySystemOptions: [
+          //   {
+          //     name: 'com.widevine.alpha',
+          //     options: {
+          //       licenseUrl: license,
+          //     },
+          //   }
+          // ]
+        })
+      }
+
+    }
+    // else {
+    //   playerRef.current.src({
+    //     src: displayInfo?.streamUrl,
+    //     type: "application/x-mpegURL",
+    //     keySystemOptions: [
+    //       {
+    //         name: 'com.apple.fps.1_0',
+    //         options: {
+    //           licenseUrl: displayInfo?.licenseUrl,
+    //         },
+    //       }
+    //     ],
+    //   })
+    // }
+
+  }
+
+
+  React.useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null
+  return (
+    <div>
+      <Test options={videoJsOptions} onReady={handlePlayerReady} />
+      <br /><br />
+
+      <input style={{ width: '400px', marginRight: '16px' }} value={url} onChange={(e) => setUrl(e.target.value)} />
+      <input style={{ width: '400px' }} value={license} onChange={(e) => setLicense(e.target.value)} />
+      <br /><br />
+
+      <button style={{  marginRight: '16px' }} onClick={() => onTestStream('dash')}>Test Stream DASH</button>
+      <button  style={{  marginRight: '16px' }} onClick={() => onTestStream('hls')}>Test Stream HLS</button>
+      <button onClick={() => { setUrl(''); setLicense('') }}>CLEAR URL</button>
+    </div >
+  );
+}
