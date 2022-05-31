8da7fbf6a565d3352c1b7d7e9e7b4d10b89f10c3
nut-parithat <starsis25391@gmail.com>
2022-05-31 12:00:41 +0700

    video-fairplay

--- route/TestIndex.js
+++ route/TestIndex.js
@@ -108,7 +108,7 @@ export default function Home() {
             {
               name: 'com.widevine.alpha',
               options: {
-                licenseUrl: license,
+                serverURL: license,
               },
             }
           ]
